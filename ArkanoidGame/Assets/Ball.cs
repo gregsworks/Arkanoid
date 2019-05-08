afcf5540fcbd2606b8cabf9c565ed37d9160ce25
gregsworks <grz.dab.97@gmail.com>
2019-05-09 00:03:01 +0200

    Zrobiłem prawie do końca trzecią instrukcję, zapis przed 25. punktem tworzenia tej gry

---
+++ ArkanoidGame/Assets/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddle1;
+
+    Vector2 paddleToBallVector;
+
+    private void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position ;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+
+    }
+}
---
+++ ArkanoidGame/Assets/Paddle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+
+    void Update()
+    {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width *16f;
+
+        Debug.Log(mousePosInUnits);
+
+        Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        transform.position = paddlePos;
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+    }
+}
+
