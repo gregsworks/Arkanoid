f61d4dfbac0cf505b2c40173f259b425185c0aaa
gregsworks <grz.dab.97@gmail.com>
2019-05-21 16:52:53 +0200

    Zrobiłem instukcję czwartą.

--- ArkanoidGame/Assets/Ball.cs
+++ ArkanoidGame/Assets/Ball.cs
@@ -1,26 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
     [SerializeField] Paddle paddle1;
-
+    [SerializeField] float xPush;
+    [SerializeField] float yPush;
+    [SerializeField] bool hasStarted = false;
+    [SerializeField] Rigidbody2D rb;
     Vector2 paddleToBallVector;
 
     private void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
-
         rb.simulated = false;
     }
 
     // Update is called once per frame
     private void Update()
     {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+    
+
+    private void LaunchOnMouseClick()
+    {
+          if (Input.GetMouseButtonDown(0));
+        {
+            hasStarted = true;
+            rb.simulated = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
-
     }
 }
